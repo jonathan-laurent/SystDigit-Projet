721c62c977635b6ca716d965c8b84837a3d7b215
Alex AUVOLAT <alex.auvolat@ens.fr>
2014-01-02 15:47:08 +0100

    Memorizing CPU : summs all the entries

--- cpu/cpu.ml
+++ cpu/cpu.ml
@@ -2,7 +2,14 @@ open Netlist_gen
 
 (* Dumb CPU that just does an 8-bit addition *)
 
+let sumz n i =
+    let rec res =
+        let aux = reg n in
+        fun () -> fst (Alu.nadder n i (aux res) (value "0")) ()
+    in
+       res
+
 let p = 
-main_2_2 (fun a b -> Alu.nadder 8 a b (value "0")) 8 8
+    main_1_1 (sumz 8) 8
 
 let () = Netlist_proc.print stdout p
